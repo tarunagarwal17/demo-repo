31238a98f852dd9a73aeaccb3fe4ed2a70c0f564
tarunagarwal17 <tarunsps@rediff.com>
2022-03-02 03:00:46 +0530

    First Commit

---
+++ src/test1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class test1 {
+	
+	public static void main(String[] args) {
+		
+		String a="yes";
+		if(a =="yes")System.out.println("cool");
+		else System.out.println("no");
+	}
+
+}
